7415101cf461f858129669fc8400669c1d415610
TheMrChaptastic <chappy@themrchaptastic.com>
2022-01-28 11:00:46 -0600

    Added RL for Cruz

--- PracticeLinq/Program.cs
+++ PracticeLinq/Program.cs
@@ -8,8 +8,8 @@ namespace PracticeLinq
     {
         static void Main(string[] args)
         {
-            List<string> videoGames = new List<string>() { "Guild Wars", "Guild Wars 2", "Final Fantasy 14", "Halo", "Osu!", "Sound Voltex"};
-            videoGames = videoGames.OrderBy(x => x.Length).ToList();
+            List<string> videoGames = new List<string>() { "Guild Wars", "Guild Wars 2", "Final Fantasy 14", "Halo", "Osu!", "Sound Voltex", "Rocket League"};
+            videoGames = videoGames.OrderBy(x => x.Length).ThenBy(x => x).ToList();
             videoGames.ForEach(x => Console.WriteLine(x));
 
         }
