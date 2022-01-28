13615137179ee2c339ab83ea9c4628154f59f380
TheMrChaptastic <chappy@themrchaptastic.com>
2022-01-28 09:42:25 -0600

    first

---
+++ PracticeLinq/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace PracticeLinq
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> videoGames = new List<string>() { "Guild Wars", "Guild Wars 2", "Final Fantasy 14", "Halo", "Osu!", "Sound Voltex"};
+            videoGames = videoGames.OrderBy(x => x.Length).ToList();
+            videoGames.ForEach(x => Console.WriteLine(x));
+
+        }
+    }
+}
